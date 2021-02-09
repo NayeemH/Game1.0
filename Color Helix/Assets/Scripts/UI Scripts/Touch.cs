f8305b276a293f76af1d653c15e03eaf25dfbf1b
Hasi-b <hasib.hasan9@gmail.com>
2021-02-09 07:20:07 +0600

    Touch script implemented

---
+++ Color Helix/Assets/Scripts/UI Scripts/Touch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Touch : MonoBehaviour, IPointerDownHandler,IPointerUpHandler
+{
+
+    private static bool pressing;
+
+    public static bool Ispressing()
+    {
+        if (pressing)
+        {
+            Debug.Log("Pressing");
+        }
+        else
+        {
+            Debug.Log("Not Pressing");
+        }
+        return pressing;
+    }
+
+    // Start is called before the first frame update
+   public void OnPointerDown(PointerEventData data)
+    {
+        pressing = true;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        pressing = false;
+    }
+}
